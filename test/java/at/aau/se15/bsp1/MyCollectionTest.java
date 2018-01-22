df977906802fccbf5ca69a7cc889f49e6500d77d
SebastianAT <setr780@gmail.com>
2018-01-23 00:44:24 +0100

    setup test for first feature

--- test/java/at/aau/se15/bsp1/MyCollectionTest.java
+++ test/java/at/aau/se15/bsp1/MyCollectionTest.java
@@ -20,4 +20,18 @@ public class MyCollectionTest
     public void testSizeSimple() {
         assertEquals(3,c.size());
     }
+    
+    @Test
+    public void testRemove() {
+    	MyCollection c1 = new MyCollection(5);
+    	c1.add("1");
+    	c1.add("2");
+    	c.remove("1");
+    	assertEquals(c1.getList(), c.getList());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testRemoveWithExc() {
+    	c1.remove("3");
+    }
 }
